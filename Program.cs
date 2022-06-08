31046de6a3e2644cdf0a5ebc589a9b1111600862
Andrew Barber <drewbarber72@gmail.com>
2022-06-08 18:22:42 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace OperatorsExercise2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Exercise 1
+            int a = 17;
+            int b = 4;
+            int quotient = a / b;
+            int remainder = a % b;
+            Console.WriteLine($"{a}/{4} is {quotient} remainder {remainder}");
+            Console.WriteLine();
+
+            //Exercise 2
+            Console.WriteLine("What is the radius of your circle?");
+            var radius = double.Parse(Console.ReadLine());
+            var area = AreaOfCircle(radius);
+            Console.WriteLine($"The area of a circle with radius of {radius} is {area}");
+        }
+
+        public static double AreaOfCircle(double radius)
+        {
+            return (Math.PI * radius * radius);
+        }
+    }
+}
